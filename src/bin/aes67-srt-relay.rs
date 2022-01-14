afee0f44250581bb634de208da1507e51982cae0
Tim-Philipp Müller <tim@centricular.com>
2022-01-14 00:29:32 +0000

    Add project skeleton

---
+++ src/bin/aes67-srt-relay.rs
@@ -0,0 +1,13 @@
+// aes67-srt-relay
+//
+// Copyright (C) 2022 Tim-Philipp Müller <tim centricular com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at
+// <https://mozilla.org/MPL/2.0/>.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+fn main() {
+    println!("Hello, world!");
+}
