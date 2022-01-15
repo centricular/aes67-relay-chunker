d13561b3b33517b0f1b90f048c70093f7962ae1a
Tim-Philipp Müller <tim@centricular.com>
2022-01-15 10:36:37 +0000

    Add rtsp-audio-server
    Little helper tool to serve some raw audio over RTSP.
    
    We don't bother with clocks and such since we won't be
    syncing to those on the client-side anyway.

---
+++ src/bin/rtsp-audio-server.rs
@@ -0,0 +1,61 @@
+// rtsp-audio-server
+//
+// Copyright (C) 2022 Tim-Philipp Müller <tim centricular com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at
+// <https://mozilla.org/MPL/2.0/>.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+use gst_rtsp_server::prelude::*;
+
+use anyhow::Error;
+
+fn main_loop() -> Result<(), Error> {
+    let main_loop = glib::MainLoop::new(None, false);
+
+    let server = gst_rtsp_server::RTSPServer::new();
+
+    let mounts = server.mount_points().unwrap();
+
+    let factory = gst_rtsp_server::RTSPMediaFactory::new();
+
+    // TODO: use more interesting audio signal than wave=ticks
+    factory.set_launch(
+        "audiotestsrc wave=ticks samplesperbuffer=48 ! audio/x-raw,rate=48000,channels=2 ! rtpL24pay name=pay0",
+    );
+
+    // Each client should get the exact same live data/stream
+    factory.set_shared(true);
+
+    // Disable RTCP (esp. Sender Reports)
+    factory.set_enable_rtcp(false);
+
+    mounts.add_factory("/audio", &factory);
+
+    // Attach the server to the default main context
+    let id = server.attach(None)?;
+
+    println!(
+        "Stream ready at rtsp://127.0.0.1:{}/audio",
+        server.bound_port()
+    );
+
+    // Start the main loop
+    main_loop.run();
+
+    id.remove();
+
+    Ok(())
+}
+
+fn main() {
+    gst::init().unwrap();
+
+    match main_loop() {
+        Ok(r) => r,
+        Err(e) => eprintln!("Error! {}", e),
+    }
+    println!("done.");
+}
