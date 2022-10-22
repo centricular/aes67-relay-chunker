ec46d901edb888fe84174c9d5c240fd052ff0bbc
Tim-Philipp Müller <tim@centricular.com>
2022-10-22 17:54:46 +0100

    mpegts: test: update for EncodedFrame change

--- src/bin/fragment_enc/mpegts/tests.rs
+++ src/bin/fragment_enc/mpegts/tests.rs
@@ -8,6 +8,8 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+use crate::EncodedFrameFormat::AacLc;
+
 #[cfg(test)]
 #[test]
 fn write_pes_183bytes_in_last_packet() {
@@ -28,22 +30,27 @@ fn write_pes_183bytes_in_last_packet() {
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33386666666)),
             buffer: gst::Buffer::from_slice(&[0x00u8; 827]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33408000000)),
             buffer: gst::Buffer::from_slice(&[0x11u8; 820]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33429333333)),
             buffer: gst::Buffer::from_slice(&[0x22u8; 846]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33450666666)),
             buffer: gst::Buffer::from_slice(&[0x33u8; 828]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33472000000)),
             buffer: gst::Buffer::from_slice(&[0x44u8; 853]),
+            format: AacLc,
         },
     ];
 
@@ -77,22 +84,27 @@ fn write_pes_no_stuffing_needed() {
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33386666666)),
             buffer: gst::Buffer::from_slice(&[0x00u8; 790]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33408000000)),
             buffer: gst::Buffer::from_slice(&[0x11u8; 853]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33429333333)),
             buffer: gst::Buffer::from_slice(&[0x22u8; 857]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33450666666)),
             buffer: gst::Buffer::from_slice(&[0x33u8; 888]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33472000000)),
             buffer: gst::Buffer::from_slice(&[0x44u8; 971]),
+            format: AacLc,
         },
     ];
     let mut cc: u8 = 8;
@@ -125,22 +137,27 @@ fn write_pes_184byte_payload_for_last_packet() {
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33386666666)),
             buffer: gst::Buffer::from_slice(&[0x00u8; 856]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33408000000)),
             buffer: gst::Buffer::from_slice(&[0x11u8; 880]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33429333333)),
             buffer: gst::Buffer::from_slice(&[0x22u8; 861]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33450666666)),
             buffer: gst::Buffer::from_slice(&[0x33u8; 835]),
+            format: AacLc,
         },
         EncodedFrame {
             pts: Some(gst::ClockTime::from_nseconds(33472000000)),
             buffer: gst::Buffer::from_slice(&[0x44u8; 934]),
+            format: AacLc,
         },
     ];
 
